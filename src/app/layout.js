dc970c29741cb1cf81c576bb2ff4322ffa6bdc81
mateusrosa <pedrompmrosa@gmail.com>
2024-05-12 17:34:34 +0100

    trybear

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+    <link rel="icon" href="/favicon.ico" />
+    </head>
       <body className={clsx(inter.variable, "bg-background text-foreground")}>
        
           {children}
